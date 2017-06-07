f7d97eaa1ad1b26e4caace5e6600d193c11879e5
John Knotts <jhnnyk@gmail.com>
2017-06-07 15:07:09 -0600

    successfully scraping

--- scraper.js
+++ scraper.js
@@ -1,8 +1,22 @@
 const fs = require('fs');
+const scrapeIt = require('scrape-it');
 
 // if the 'data' directory does not exist, create it
 fs.stat("data/", (err, stats) => {
   if (err) {
     fs.mkdirSync("data/");
   }
+});
+
+// scrape the webpage
+scrapeIt("http://www.irunfar.com/", {
+    articles: {
+        listItem: ".noFeat",
+        data: {
+            title: "h5",
+            excerpt: ".excerpt"
+        }
+    }
+}).then(page => {
+    console.log(page);
 });
