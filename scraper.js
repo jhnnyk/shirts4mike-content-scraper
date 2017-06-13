b4d146a88e6ca45e0e84d9751b8169451cd4483c
John Knotts <jhnnyk@gmail.com>
2017-06-13 11:18:00 -0600

    trying out standard-js

--- scraper.js
+++ scraper.js
@@ -1,64 +1,64 @@
-const fs = require('fs');
-const scrapeIt = require('scrape-it');
-const converter = require('json-2-csv');
+const fs = require('fs')
+const scrapeIt = require('scrape-it')
+const converter = require('json-2-csv')
 
-let shirtsArray = [];
-let numberOfShirts = 0;
+let shirtsArray = []
+let numberOfShirts = 0
 
 // if the 'data' directory does not exist, create it
-fs.stat("data/", (err, stats) => {
+fs.stat('data/', (err, stats) => {
   if (err) {
-    fs.mkdirSync("data/");
+    fs.mkdirSync('data/')
   }
-});
+})
 
 // scrape the index page to get the URLs for each shirt
-scrapeIt("http://www.shirts4mike.com/shirts.php", {
+scrapeIt('http://www.shirts4mike.com/shirts.php', {
   shirts: {
-    listItem: ".products li",
+    listItem: '.products li',
     data: {
       url: {
-        selector: "a",
-        attr: "href"
+        selector: 'a',
+        attr: 'href'
       }
     }
   }
 }).then(shirtURLs => {
-  shirtURLs.shirts.forEach(scrapeShirt);
-  numberOfShirts = shirtURLs.shirts.length;
-});
+  shirtURLs.shirts.forEach(scrapeShirt)
+  numberOfShirts = shirtURLs.shirts.length
+})
 
 // scrape info for each individual shirt
 const scrapeShirt = (shirt) => {
-  let shirtURL = "http://www.shirts4mike.com/" + shirt.url;
+  let shirtURL = 'http://www.shirts4mike.com/' + shirt.url
 
   scrapeIt(shirtURL, {
-    title: "title",
-    price: "h1 span",
+    title: 'title',
+    price: 'h1 span',
     imgURL: {
-      selector: ".shirt-picture img",
-      attr: "src"
+      selector: '.shirt-picture img',
+      attr: 'src'
     }
   }).then(shirt => {
-    shirt.url = shirtURL;
-    shirt.time = new Date().toString();
-    shirtsArray.push(shirt);
+    shirt.url = shirtURL
+    shirt.time = new Date().toString()
+    shirtsArray.push(shirt)
     if (numberOfShirts === shirtsArray.length) {
-      convert2csv();
+      convert2csv()
     }
-  });
-};
+  })
+}
 
 // go through the shirts array and parse it as CSV
 const convert2csv = function () {
-  converter.json2csv(shirtsArray, write2csv);
-};
+  converter.json2csv(shirtsArray, write2csv)
+}
 
 // write the CSV file
 const write2csv = function (err, csv) {
-  if (err) throw err;
+  if (err) throw err
   fs.writeFile('data/today.csv', csv, (error) => {
-    if (error) throw error;
-    console.log('The file has been saved!');
-  });
-};
+    if (error) throw error
+    console.log('The file has been saved!')
+  })
+}
